54b5fdfc6cbf3150590f5cba35d2ef1a900f0d26
Natalie - Laptop <natalie1vm@hotmail.com>
2018-06-26 21:57:31 -0400

    Level 1 and scene manager

---
+++ Assets/Scripts/clickSelect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class clickSelect : MonoBehaviour {
+		public Material on;
+		public Material off;
+		public String sceneName;
+		void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+				this.gameObject.GetComponent<MeshRenderer>().material = on;
+				if (Input.GetMouseButtonDown(0))
+				{
+					if(sceneName == "quit")
+					{
+						Debug.Log("Quit application");
+						Application.Quit();
+					}
+					SceneManager.LoadScene(sceneName);
+				}
+    }
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+				this.gameObject.GetComponent<MeshRenderer>().material = off;
+    }
+}
--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerManager : MonoBehaviour {
 	public Light lightSource;
-	public ParticleSystem fire;
+	public GameObject lightsOut;
+	public GameObject back;
+	public ParticleSystem fire_ps;
+	public int levelTime;
 	private int alpha;
 	// Use this for initialization
 	void Start () {
 		lightSource.GetComponent<Light>();
+		lightsOut.SetActive(false);
+		back.SetActive(true);
 	}
 
 	// Update is called once per frame
@@ -16,12 +22,23 @@ public class playerManager : MonoBehaviour {
     {
         var z = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-        fire.transform.Rotate(0, 0, z);
-        fire.transform.Translate(0, -y, 0);
+        fire_ps.transform.Rotate(0, z, 0);
+        fire_ps.transform.Translate(0, 0, y);
 
 				lightSource.intensity -= 0.002f;
 
-				alpha --;
-				fire.main.startColor = new Color(255, 185, 0, alpha);
+				if(Time.timeSinceLevelLoad > levelTime - 2) {
+					fire_ps.Stop();
+					if(Time.timeSinceLevelLoad > levelTime)
+					{
+						lightsOut.SetActive(true);
+						back.SetActive(false);
+					}
+					if(Time.timeSinceLevelLoad > levelTime + 3)
+						SceneManager.LoadScene("loseScreen");
+				}
+
+				// alpha --;
+				// fire.main.startColor = new Color(255, 185, 0, alpha);
     }
 }
